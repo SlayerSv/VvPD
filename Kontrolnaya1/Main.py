59996d1d01864e84bf7d23c5e5015cc4457e4189
SlayerSv <slayer-sv@mail.ru>
2023-05-13 18:35:13 +0700

    Добавлена проверка ввода пользователя

--- Kontrolnaya1/Main.py
+++ Kontrolnaya1/Main.py
@@ -8,19 +8,26 @@ def main():
     print('\nДля этого вам нужно ввести длины двух сторон треугольника и угол')
     print('между ними. Для того, чтобы выйти из программы введите "0"\n')
     while True:
-        first_side = int(input('Введите длину первой стороны (0 = exit): '))
+        message = 'Введите длину первой стороны (0 = exit): '
+        first_side = input_check(message, 0, 999999)
         if first_side == 0:
             return
-        second_side = int(input('Введите длину второй стороны (0 = exit): '))
+        message = 'Введите длину второй стороны (0 = exit): '
+        second_side = input_check(message, 0, 999999)
         if second_side == 0:
             return
-        angle_deg = (int(input('Введите угол между сторонами (0 = exit):')))
+        message = 'Введите угол между сторонами в градусах: '
+        angle_deg = input_check(message, 0, 180)
         if angle_deg == 0:
             return
         angle_rad = m.radians(angle_deg)
+        print(angle_rad)
+        print(m.cos(angle_rad))
+        print(round(m.cos(angle_rad), 1))
         third_side = m.sqrt(m.pow(first_side, 2) + m.pow(second_side, 2) - 2
                             * first_side * second_side
-                            * round(m.cos(angle_rad), 2))
+                            * round(m.cos(angle_rad), 5))
+        print(third_side)
         third_side = round(third_side, 2)
         print('Третья сторона треугольника равна ' + str(third_side))
         if first_side == second_side == third_side:
@@ -44,26 +51,44 @@ def is_right_triangle(first_side, second_side, third_side):
     if first_side > second_side:
         if third_side > first_side:
             hypotenuse = third_side
-            leg_a = first_side
-            leg_b = second_side
+            katet_a = first_side
+            katet_b = second_side
         else:
             hypotenuse = first_side
-            leg_a = second_side
-            leg_b = third_side
+            katet_a = second_side
+            katet_b = third_side
     else:
         if third_side > second_side:
             hypotenuse = third_side
-            leg_a = first_side
-            leg_b = second_side
+            katet_a = first_side
+            katet_b = second_side
         else:
             hypotenuse = second_side
-            leg_a = first_side
-            leg_b = third_side
-    if m.sqrt(m.pow(hypotenuse, 2)) == m.sqrt(m.pow(leg_a, 2)
-                                              + m.pow(leg_b, 2)):
+            katet_a = first_side
+            katet_b = third_side
+    if m.sqrt(m.pow(hypotenuse, 2)) == m.sqrt(m.pow(katet_a, 2)
+                                              + m.pow(katet_b, 2)):
         return True
     else:
         return False
+
+
+def input_check(message, min_value, max_value):
+    while True:
+        try:
+            user_input = input(message)
+            user_input = int(user_input)
+        except ValueError:
+            try:
+                user_input = float(user_input)
+            except ValueError:
+                print('Нужно ввести число!')
+                continue
+        if user_input < min_value or user_input >= max_value:
+            print('Число должно быть между ' + str(min_value)
+                  + ' и ' + str(max_value))
+            continue
+        return user_input
 
 
 if __name__ == '__main__':
