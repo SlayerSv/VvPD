687f9938e0350a0fa9946969d85147d82093c4a4
SlayerSv <slayer-sv@mail.ru>
2023-05-16 16:40:23 +0700

    kontrolnaya 2 added need_to_move function

--- Kontrolnaya2/main.py
+++ Kontrolnaya2/main.py
@@ -3,18 +3,20 @@ def main():
         brackets = input('Введите скобочную последовательность: ')
         if brackets == '0':
             return
-        if check_brackets(brackets):
+        if is_cbs(brackets):
             print('Скобочная последовательность правильная!')
         else:
             print('Скобочная последовательность неправильная :(')
+            print('Нужно переместить ' + str(need_to_move(brackets))
+                  + ' скобок!')
 
 
-def check_brackets(brackets):
+def is_cbs(brackets):
     opening_brackets_count = 0
-    for i in brackets:
-        if i == '(':
+    for c in brackets:
+        if c == '(':
             opening_brackets_count += 1
-        elif i == ')':
+        elif c == ')':
             if opening_brackets_count > 0:
                 opening_brackets_count -= 1
             else:
@@ -25,6 +27,42 @@ def check_brackets(brackets):
         return True
     else:
         raise ValueError('Недостаточно закрывающихся скобок :(')
+
+
+def need_to_move(input):
+    brackets = input
+    opening_brackets_count = 0
+    moved_brackets = 0
+    slice_brackets1 = ''
+    slice_brackets2 = ''
+    is_done = False
+    has_moved = False
+    i = 0
+    while is_done is False:
+        for c in brackets:
+            has_moved = False
+            if c == '(':
+                opening_brackets_count += 1
+                i += 1
+            elif c == ')':
+                if opening_brackets_count > 0:
+                    opening_brackets_count -= 1
+                    i += 1
+                else:
+                    slice_brackets1 = brackets[:i]
+                    print(slice_brackets1)
+                    slice_brackets2 = brackets[i + 1:]
+                    print(slice_brackets2)
+                    brackets = slice_brackets1 + slice_brackets2 + ')'
+                    moved_brackets += 1
+                    i = 0
+                    opening_brackets_count = 0
+                    print(brackets)
+                    has_moved = True
+                    break
+        if has_moved is False:
+            is_done = True
+    return moved_brackets
 
 
 if __name__ == '__main__':
