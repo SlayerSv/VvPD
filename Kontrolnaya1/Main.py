17d6264f5815ee561e63e45c3b45631a5d008698
SlayerSv <slayer-sv@mail.ru>
2023-05-17 09:56:20 +0700

    Kontrolnaya1: code split, formula rounding

--- Kontrolnaya1/Main.py
+++ Kontrolnaya1/Main.py
@@ -8,43 +8,22 @@ def main():
     print('\nДля этого вам нужно ввести длины двух сторон треугольника и угол')
     print('между ними. Для того, чтобы выйти из программы введите "0"\n')
     while True:
-        message = 'Введите длину первой стороны (0 = exit): '
-        first_side = input_check(message, 0, 999999)
+        message = 'Введите длину первой стороны (0 для выхода): '
+        first_side = input_check(message, 0, 9999999)
         if first_side == 0:
             return
-        message = 'Введите длину второй стороны (0 = exit): '
-        second_side = input_check(message, 0, 999999)
+        message = 'Введите длину второй стороны (0 для выхода): '
+        second_side = input_check(message, 0, 9999999)
         if second_side == 0:
             return
-        message = 'Введите угол между сторонами в градусах: '
+        message = 'Введите угол между сторонами в градусах (0 для выхода): '
         angle_deg = input_check(message, 0, 180)
         if angle_deg == 0:
             return
         angle_rad = m.radians(angle_deg)
-        print(angle_rad)
-        print(m.cos(angle_rad))
-        print(round(m.cos(angle_rad), 1))
-        third_side = m.sqrt(m.pow(first_side, 2) + m.pow(second_side, 2) - 2
-                            * first_side * second_side
-                            * round(m.cos(angle_rad), 5))
-        print(third_side)
-        third_side = round(third_side, 2)
+        third_side = calculate_third_side(first_side, second_side, angle_rad)
         print('Третья сторона треугольника равна ' + str(third_side))
-        if first_side == second_side == third_side:
-            print('Все стороны равны! Это равносторонний треугольник!')
-            continue
-        elif is_right_triangle(first_side, second_side, third_side):
-            print('Квадрат гипотенузы равен сумме квадратов катетов!')
-            print('Это прямоугольный треугольник!')
-            if (first_side == second_side) or (first_side == third_side) \
-                    or (second_side == third_side):
-                print('Две стороны равны! Это равнобедренный треугольник!')
-        elif (first_side == second_side) or (first_side == third_side) \
-                or (second_side == third_side):
-            print('Две стороны равны! Это равнобедренный треугольник!')
-        else:
-            print('Треугольник не является ни равносторонним,')
-            print('ни равнобедренным, ни прямоугольным :(')
+        define_triangle(first_side, second_side, third_side)
 
 
 def is_right_triangle(first_side, second_side, third_side):
@@ -66,8 +45,9 @@ def is_right_triangle(first_side, second_side, third_side):
             hypotenuse = second_side
             katet_a = first_side
             katet_b = third_side
-    if m.sqrt(m.pow(hypotenuse, 2)) == m.sqrt(m.pow(katet_a, 2)
-                                              + m.pow(katet_b, 2)):
+    print()
+    if m.sqrt(round(m.pow(hypotenuse, 2), 1)) == m.sqrt(m.pow(katet_a, 2)
+                                                        + m.pow(katet_b, 2)):
         return True
     else:
         return False
@@ -89,6 +69,29 @@ def input_check(message, min_value, max_value):
                   + ' и ' + str(max_value))
             continue
         return user_input
+
+
+def calculate_third_side(first_side, second_side, angle_radians):
+    return round(m.sqrt(m.pow(first_side, 2) + m.pow(second_side, 2) - 2
+                 * first_side * second_side
+                 * m.cos(angle_radians)), 2)
+
+
+def define_triangle(first_side, second_side, third_side):
+    if first_side == second_side == third_side:
+        print('Все стороны равны! Это равносторонний треугольник!')
+    elif is_right_triangle(first_side, second_side, third_side):
+        print('Квадрат гипотенузы равен сумме квадратов катетов!')
+        print('Это прямоугольный треугольник!')
+        if (first_side == second_side) or (first_side == third_side) \
+                or (second_side == third_side):
+            print('Две стороны равны! Это равнобедренный треугольник!')
+    elif (first_side == second_side) or (first_side == third_side) \
+            or (second_side == third_side):
+        print('Две стороны равны! Это равнобедренный треугольник!')
+    else:
+        print('Треугольник не является ни равносторонним,')
+        print('ни равнобедренным, ни прямоугольным :(')
 
 
 if __name__ == '__main__':
