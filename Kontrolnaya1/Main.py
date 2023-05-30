7b696f227fe326ac69aeed6e8181a03eb3722fae
SlayerSv <slayer-sv@mail.ru>
2023-05-30 07:40:00 +0700

    Rounding can be passed as an argument

--- Kontrolnaya1/Main.py
+++ Kontrolnaya1/Main.py
@@ -7,6 +7,10 @@ def main(args):
         first_side = input_check_side(args[1])
         second_side = input_check_side(args[2])
         angle_deg = input_check_angle(args[3])
+        if len(args) > 4:
+            rounding = input_check_rounding(args[4])
+        else:
+            rounding = 2
     except ValueError as exception:
         print(exception)
         return
@@ -16,11 +20,11 @@ def main(args):
         return
     angle_rad = m.radians(angle_deg)
     third_side = calculate_third_side(first_side, second_side, angle_rad)
-    print("Third side's length is: " + str(round(third_side, 2)))
-    define_triangle(first_side, second_side, third_side)
+    print("Third side's length is: " + str(round(third_side, rounding)))
+    define_triangle(first_side, second_side, third_side, rounding)
 
 
-def is_right_triangle(first_side, second_side, third_side):
+def is_right_triangle(first_side, second_side, third_side, rounding):
     if first_side > second_side:
         if third_side > first_side:
             hypotenuse = third_side
@@ -40,8 +44,8 @@ def is_right_triangle(first_side, second_side, third_side):
             katet_a = first_side
             katet_b = third_side
     print()
-    if m.sqrt(round(m.pow(hypotenuse, 2), 2)) == m.sqrt(m.pow(katet_a, 2)
-                                                        + m.pow(katet_b, 2)):
+    if m.sqrt(round(m.pow(hypotenuse, 2), rounding))\
+            == m.sqrt(m.pow(katet_a, 2) + m.pow(katet_b, 2)):
         return True
     else:
         return False
@@ -67,23 +71,35 @@ def input_check_angle(angle):
     return angle
 
 
+def input_check_rounding(rounding):
+    try:
+        rounding = int(rounding)
+    except ValueError as exception:
+        raise ValueError("Rounding value must be a number!") from exception
+    if rounding <= 0:
+        raise ValueError("Rounding value must be greater than zero!")
+    return rounding
+
+
 def calculate_third_side(first_side, second_side, angle_radians):
     return m.sqrt(m.pow(first_side, 2) + m.pow(second_side, 2) - 2
                   * first_side * second_side
                   * m.cos(angle_radians))
 
 
-def define_triangle(first_side, second_side, third_side):
-    if round(first_side, 2) == round(second_side, 2) == round(third_side, 3):
+def define_triangle(first_side, second_side, third_side, rounding):
+    if first_side == second_side == round(third_side, rounding):
         print("All sides are equal! This is an equilateral triangle!")
-    elif is_right_triangle(first_side, second_side, third_side):
+    elif is_right_triangle(first_side, second_side, third_side, rounding):
         print("Power of hypotenuse equals the sum of powers of the other "
               + "sides! This is a right triangle!")
-        if (first_side == second_side) or (first_side == third_side) \
-                or (second_side == third_side):
+        if (first_side == second_side)\
+                or (first_side == round(third_side, rounding))\
+                or (second_side == round(third_side, rounding)):
             print("Two sides are equal! This is an isosceles triangle!")
-    elif (first_side == second_side) or (first_side == third_side) \
-            or (second_side == third_side):
+    elif (first_side == second_side)\
+            or (first_side == round(third_side, rounding))\
+            or (second_side == round(third_side, rounding)):
         print("Two sides are equal! This is an isosceles triangle!")
     else:
         print("Triangle is not equilateral, isosceles or right :(")
